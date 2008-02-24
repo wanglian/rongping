aa9cb08caa1d3d80ccd9eee4e24bd3e01ca5342a
Ariejan de Vroom <ariejan@ariejan.net>
2008-02-24 23:30:30 +0100

    Added editing of profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,26 +1,37 @@
 class ProfilesController < ApplicationController
-  before_filter :find_user
-  
-  # TODO: (base_app) enable inline editing of the profile for the owner
+  before_filter :find_profile
+  before_filter :check_owner_access, :only => [:edit, :update]
   
   def show
-    @profile = @user.nil? ? nil : @user.profile
+    # render show.html.erb
   end
   
   def edit
-    redirect_to profile_url(params[:id]) if logged_in? && current_user != @user
+    # render edit.html.erb
   end
   
-  def update
-    redirect_to profile_url(params[:id]) if logged_in? && current_user != @user
-    # TODO: (base_app) Write profile update code
+  def update    
+    unless @profile.nil?
+      @profile.update_attributes(params[:profile])
+      flash[:notice] = "Your profile has been succesfully updated."
+      redirect_to profile_url(@profile.user)
+    else
+      render :edit
+    end
   end
   
   protected
 
-  def find_user
-    @user = User.find(params[:id])
-  rescue
-    @user = nil
+  def find_profile
+    begin
+      @user = User.find(params[:id])
+    rescue
+      @user = nil
+    end
+    @profile = @user.nil? ? nil : @user.profile
   end    
+  
+  def check_owner_access
+    redirect_to profile_url(params[:id]) if logged_in? && current_user != @user
+  end
 end
