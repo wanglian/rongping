f9006fe6f5ec6d98ba38659f9a5dbc05a2105d27
Ariejan de Vroom <ariejan@ariejan.net>
2008-02-24 14:54:45 +0100

    Imporved layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,42 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Yield the content for a given block. If the block yiels nothing, the optionally specified default text is shown. 
+  #
+  #   yield_or_default(:user_status)
+  #
+  #   yield_or_default(:sidebar, "Sorry, no sidebar")
+  #
+  # +target+ specifies the object to yield.
+  # +default_message+ specifies the message to show when nothing is yielded. (Default: "")
+  def yield_or_default(message, default_message = "")
+    message.nil? ? default_message : message
+  end
+  
+  # Create tab.
+  # 
+  # The tab will link to the first options hash in the all_options array,
+  # and make it the current tab if the current url is any of the options
+  # in the same array.
+  # 
+  # +name+ specifies the name of the tab
+  # +all_options+ is an array of hashes, where the first hash of the array is the tab's link and all others will make the tab show up as current.
+  # 
+  # If now options are specified, the tab will point to '#', and will never have the 'active' state.
+  def tab_to(name, all_options = nil)
+      url = all_options.is_a?(Array) ? all_options[0].merge({:only_path => false}) : "#"
+      
+      current_url = url_for(:action => @current_action, :only_path => false)
+      html_options = {}
+      
+      if all_options.is_a?(Array)
+        all_options.each do |o|
+          if url_for(o.merge({:only_path => false})) == current_url
+            html_options = {:class => "current"}
+            break
+          end
+        end  
+      end
+      
+      link_to(name, url, html_options)
+    end
+  
 end
