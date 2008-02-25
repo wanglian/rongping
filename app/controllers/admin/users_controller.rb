bc298994b16cf906c1be8d7321205f476be4a89c
Ariejan de Vroom <ariejan@ariejan.net>
2008-02-25 16:21:56 +0100

    Don't edit an account directly

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -88,11 +88,6 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  # GET /admin_users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
   # POST /admin_users
   # POST /admin_users.xml
   def create
@@ -105,23 +100,6 @@ class Admin::UsersController < ApplicationController
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /admin_users/1
-  # PUT /admin_users/1.xml
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        flash[:notice] = 'User was successfully updated.'
-        format.html { redirect_to(@user) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
     end
